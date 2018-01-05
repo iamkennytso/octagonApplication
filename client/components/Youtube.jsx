14bfd0c77e6f0cd90219acc9538eb6d815348d6c
iamkennytso <iamkennytso@gmail.com>
2018-01-05 15:51:42 -0500

    (feat) youtube live data

--- client/components/Youtube.jsx
+++ client/components/Youtube.jsx
@@ -1,49 +1,78 @@
 import React from 'react';
+import axios from 'axios'
 import {List, ListItem} from 'material-ui/List';
 
-const youtubeSec = (props) => {
-  return (
-    <div id="youtubeContainer">
-      <div id="youtubePlayer">
-        <iframe  type="text/html" width="95%" height="95%" allowFullScreen ="allowfullscreen"
-          src="http://www.youtube.com/embed/j90HdKkqpGY"
-          frameBorder="0">
-        </iframe>
-      </div>
-      <List id="youtubeList1" style={{'height':'100%'}}>
-        <ListItem className="youtubePrevTop" >
-          <div style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </div>
-          <div style={{'height':'auto'}}> 
-            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
-          </div>
-        </ListItem>
-        <ListItem className="youtubePrevBot">
-          <span style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </span>
-          <div style={{'height':'auto'}}> 
-            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
-          </div>
-        </ListItem>
-      </List>
+class youtubeSec extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [{title:'',thumb:''}, {title:'',thumb:''}, {title:'',thumb:''}, {title:'',thumb:''}, ],
+      currentVideo: 'xdKT_gn9Szc',
+    }
+    this.getVideos = this.getVideos.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  componentDidMount(){
+    this.getVideos()
+  }
+  
+  getVideos() {
+    axios.get('getYoutube')
+      .then(res => this.setState({
+        videos: res.data,
+        currentVideo: res.data[0].URL
+      }))
+  }
+  handleClick(num){
+    this.setState({
+      currentVideo: this.state.videos[num].URL
+    })
+  }
 
-      <List id="youtubeList2">
-        <ListItem className="youtubePrevTop" onClick={()=>console.log('hello')}>
-          <span style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </span>
-          <div style={{'height':'auto'}}> 
-            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
-          </div>
-        </ListItem>
-        <ListItem className="youtubePrevBot">
-          <span style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </span>
-          <div style={{'height':'auto'}}> 
-            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
-          </div>
-        </ListItem>
-      </List>
-      
-      <img id="youtubeBG" src="./imgs/youtubeBG.jpg" alt="Visit Doublelift's Youtube!"  width="100%"/>
-      <p id="youtubeText"> Phasellus elementum, massa ac tristique aliquam, ex tellus commodo neque, sit amet rutrum tellus risus vitae arcu. Aliquam erat volutpat. Aenean sed justo vitae augue vulputate consectetur. Aenean condimentum blandit quam sit amet auctor. Aenean euismod, lacus et commodo viverra, massa felis ullamcorper justo, id consectetur libero risus id nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi varius mi ac urna consequat rhoncus. Pellentesque condimentum mauris et ante vestibulum, ac scelerisque lectus tempor. Sed vehicula aliquam metus. </p>
-    </div>
-  )
+  render (){
+    return (
+      <div id="youtubeContainer">
+        <div id="youtubePlayer">
+          <iframe  type="text/html" width="95%" height="95%" allowFullScreen ="allowfullscreen"
+            src={`http://www.youtube.com/embed/${this.state.currentVideo}`}
+            frameBorder="0">
+          </iframe>
+        </div>
+        <List id="youtubeList1" style={{'height':'100%'}}>
+          <ListItem className="youtubePrevTop" onClick={()=> this.handleClick(0)}>
+            <div style={{'color':'#FFFFFF'}}> {this.state.videos[0].title} </div>
+            <div style={{'height':'auto'}}> 
+              <img src={this.state.videos[0].thumb} style={{'width':'100%'}} alt="" />
+            </div>
+          </ListItem>
+          <ListItem className="youtubePrevBot" onClick={()=> this.handleClick(1)}>
+            <span style={{'color':'#FFFFFF'}}> {this.state.videos[1].title} </span>
+            <div style={{'height':'auto'}}> 
+              <img src={this.state.videos[1].thumb} style={{'width':'100%'}} alt="" />
+            </div>
+          </ListItem>
+        </List>
+
+        <List id="youtubeList2">
+          <ListItem className="youtubePrevTop" onClick={()=> this.handleClick(2)}>
+            <span style={{'color':'#FFFFFF'}}> {this.state.videos[2].title} </span>
+            <div style={{'height':'auto'}}> 
+              <img src={this.state.videos[2].thumb} style={{'width':'100%'}} alt="" />
+            </div>
+          </ListItem>
+          <ListItem className="youtubePrevBot" onClick={()=> this.handleClick(3)}>
+            <span style={{'color':'#FFFFFF'}}> {this.state.videos[3].title} </span>
+            <div style={{'height':'auto'}}> 
+              <img src={this.state.videos[3].thumb} style={{'width':'100%'}} alt="" />
+            </div>
+          </ListItem>
+        </List>
+        
+        <img id="youtubeBG" src="./imgs/youtubeBG.jpg" alt="Visit Doublelift's Youtube!"  width="100%"/>
+        <p id="youtubeText"> Phasellus elementum, massa ac tristique aliquam, ex tellus commodo neque, sit amet rutrum tellus risus vitae arcu. Aliquam erat volutpat. Aenean sed justo vitae augue vulputate consectetur. Aenean condimentum blandit quam sit amet auctor. Aenean euismod, lacus et commodo viverra, massa felis ullamcorper justo, id consectetur libero risus id nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi varius mi ac urna consequat rhoncus. Pellentesque condimentum mauris et ante vestibulum, ac scelerisque lectus tempor. Sed vehicula aliquam metus. </p>
+      </div>
+    )
+  }
 }
 
 export default youtubeSec;
--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const compress = require('compression')
+const axios = require('axios')
+const env = require('dotenv').config();
+
 let app = express();
 let port = process.env.PORT || 1203
 
@@ -9,4 +12,27 @@ app.use(compress())
 app.use(bodyParser.json())
 app.listen(port, function() {
 	console.log(`<('.'<) Server's up on port 1203!`);
+})
+
+app.get('/getYoutube', (req, out) => {
+  axios.get('https://www.googleapis.com/youtube/v3/search', {
+    params: {
+      key: process.env.YOUTUBE_KEY,
+      channelId:'UCrPCP1oaOr0AEs2JdxzfOFA',
+      part:'snippet',
+      order:'date',
+      maxResults:4
+    }
+  })
+    .then(res=> {
+      const arr = []
+      for (let i = 0; i < res.data.items.length; i++){
+        let obj = {}
+        obj.URL = res.data.items[i].id.videoId
+        obj.title = res.data.items[i].snippet.title
+        obj.thumb = res.data.items[i].snippet.thumbnails.medium.url
+        arr.push(obj)
+      }
+      out.send(arr)
+    })
 })
