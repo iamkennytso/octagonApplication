534148c9108d9b473c5bd45c6e8106a2ae590f47
iamkennytso <iamkennytso@gmail.com>
2018-01-05 00:17:05 -0500

    (feat) redid footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 const footer = (props) => {
   return (
     <div id="footerDiv">
-      <img id="footerPic" src='./imgs/footer.jpg' alt="footer"/>
+      <p>
+      {/* <img id="footerPic" src='./imgs/footer.jpg' alt="footer"/>
       <a href="https://twitter.com/TLdoublelift" id="twitterLink"/>
       <a href="https://www.youtube.com/Doublelift" id="youtubeLink"/>
       <a href="https://www.instagram.com/yiliangpeng/" id="instaLink"/>
       <a href="https://www.facebook.com/DoubleliftOfficial/" id="fbLink"/>
+      <div id="footerOtherBack" /> */}
+        <a href="#" >Contact Us</a> | 
+        <a href="#" > Legal Notice</a> | 
+        <a href="#" > Terms of Use</a> 
+        <br/> 
+        All media belongs to their respective owners. This site is made for educational purposes. All rights reserved.&nbsp;&nbsp;
+      </p>
     </div>
   )
 }
