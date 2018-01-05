3bc0338a4f67766b744990c9f0b8a9094aed14fa
iamkennytso <iamkennytso@gmail.com>
2018-01-05 14:03:59 -0500

    (style) youtube component

--- client/components/Youtube.jsx
+++ client/components/Youtube.jsx
@@ -10,30 +10,36 @@ const youtubeSec = (props) => {
           frameBorder="0">
         </iframe>
       </div>
-      <div id="youtubeList1">
-        <List>
-          <ListItem>
-          <p style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </p><br/>
-          <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" height="100%" alt="" />
-          </ListItem>
-          <ListItem>
-          <p style={{'color':'#FFFFFF'}}>Doublelift - He's really not worth any gold now..." </p> <br/>
-          <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" height="100%" alt="" />
-          </ListItem>
-        </List>
-      </div>
-      <div id="youtubeList2">
-        <List>
-          <ListItem>
-          <p style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </p><br/>
-          <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" height="100%" alt="" />
-          </ListItem>
-          <ListItem>
-          <p style={{'color':'#FFFFFF'}}>Doublelift - He's really not worth any gold now..." </p> <br/>
-          <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" height="100%" alt="" />
-          </ListItem>
-        </List>
-      </div>
+      <List id="youtubeList1" style={{'height':'100%'}}>
+        <ListItem className="youtubePrevTop" >
+          <div style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </div>
+          <div style={{'height':'auto'}}> 
+            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
+          </div>
+        </ListItem>
+        <ListItem className="youtubePrevBot">
+          <span style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </span>
+          <div style={{'height':'auto'}}> 
+            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
+          </div>
+        </ListItem>
+      </List>
+
+      <List id="youtubeList2">
+        <ListItem className="youtubePrevTop" onClick={()=>console.log('hello')}>
+          <span style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </span>
+          <div style={{'height':'auto'}}> 
+            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
+          </div>
+        </ListItem>
+        <ListItem className="youtubePrevBot">
+          <span style={{'color':'#FFFFFF'}}> Doublelift - He's really not worth any gold now..." </span>
+          <div style={{'height':'auto'}}> 
+            <img src="https://i.ytimg.com/vi/_a8VKtrFnAc/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1i6c3pl0yqHdWMU_bFR45UIThPQ" style={{'width':'100%'}} alt="" />
+          </div>
+        </ListItem>
+      </List>
+      
       <img id="youtubeBG" src="./imgs/youtubeBG.jpg" alt="Visit Doublelift's Youtube!"  width="100%"/>
       <p id="youtubeText"> Phasellus elementum, massa ac tristique aliquam, ex tellus commodo neque, sit amet rutrum tellus risus vitae arcu. Aliquam erat volutpat. Aenean sed justo vitae augue vulputate consectetur. Aenean condimentum blandit quam sit amet auctor. Aenean euismod, lacus et commodo viverra, massa felis ullamcorper justo, id consectetur libero risus id nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi varius mi ac urna consequat rhoncus. Pellentesque condimentum mauris et ante vestibulum, ac scelerisque lectus tempor. Sed vehicula aliquam metus. </p>
     </div>
