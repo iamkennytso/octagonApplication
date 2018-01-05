3e3e4332059d6c5f7bd73297327ae7bf478d323f
iamkennytso <iamkennytso@gmail.com>
2018-01-04 23:42:24 -0500

    (feat) hero image

--- client/index.jsx
+++ client/index.jsx
@@ -3,7 +3,6 @@ import ReactDom from 'react-dom';
 import { Element } from 'react-scroll'
 
 import Navbar from './components/Navbar.jsx'
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +16,7 @@ class App extends React.Component {
       <div>
         <Navbar />          
 
+        <img className="section" src="./imgs/hero.jpg" alt="DoubleliftIsTheBest!" width="100%"/>
         <Element className="section" name='sec1'>
           <div>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget purus diam. Sed porta sapien quis quam ultrices volutpat. Sed vel justo lacinia, varius justo vitae, mattis felis. Sed leo dui, sagittis sed urna a, vehicula blandit massa. Aenean euismod porta arcu ac ullamcorper. Integer urna massa, tristique a rhoncus at, commodo vel purus. Nam id diam dapibus, vestibulum eros et, porta mauris. Pellentesque vitae tellus iaculis nisi pellentesque hendrerit id vel turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam lacinia sem mi, sit amet malesuada ex commodo in. Curabitur fermentum lectus quis erat convallis sollicitudin. Nulla fermentum vehicula finibus. Morbi non sagittis risus. In vulputate mi lacus, at bibendum mi posuere nec.
