d3f6ab982732bee7b864217f6d6c040b1529e0f9
iamkennytso <iamkennytso@gmail.com>
2018-01-05 00:31:35 -0500

    (feat) footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,19 +3,30 @@ import React from 'react';
 const footer = (props) => {
   return (
     <div id="footerDiv">
-      <p>
       {/* <img id="footerPic" src='./imgs/footer.jpg' alt="footer"/>
       <a href="https://twitter.com/TLdoublelift" id="twitterLink"/>
       <a href="https://www.youtube.com/Doublelift" id="youtubeLink"/>
       <a href="https://www.instagram.com/yiliangpeng/" id="instaLink"/>
       <a href="https://www.facebook.com/DoubleliftOfficial/" id="fbLink"/>
       <div id="footerOtherBack" /> */}
-        <a href="#" >Contact Us</a> | 
-        <a href="#" > Legal Notice</a> | 
-        <a href="#" > Terms of Use</a> 
-        <br/> 
-        All media belongs to their respective owners. This site is made for educational purposes. All rights reserved.&nbsp;&nbsp;
-      </p>
+      <div id="footerLeft">
+        <p>
+          <br/>
+          <a href="#" >Contact Us</a> | 
+          <a href="#" > Legal Notice</a> | 
+          <a href="#" > Terms of Use</a> 
+          <br/> 
+          All media belongs to their respective owners.<br/> This site is made for educational purposes. <br/> All rights reserved.&nbsp;&nbsp;
+        </p>
+      </div>
+      <div id="footerRight">
+        <p>
+          <img src="./imgs/twitter.png" /> | <a href="https://twitter.com/TLdoublelift">TLDoublelift</a> <br/>
+          <img src="./imgs/youtube.png" /> | <a href="https://www.youtube.com/Doublelift">Doublelift</a> <br/>
+          <img src="./imgs/insta.png" /> | <a href="https://www.instagram.com/yiliangpeng/">YiLiangPeng</a> <br/>
+          <img src="./imgs/facebook.png" /> | <a href="https://www.facebook.com/DoubleliftOfficial/">DoubleliftOfficial</a> <br/>
+        </p>
+      </div>
     </div>
   )
 }
